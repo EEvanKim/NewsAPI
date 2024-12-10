2134164af3f85dd645682dbeaeb6b64dcd1858a9
Evan Kim <jekkej08@gmail.com>
2024-12-10 14:35:49 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Evan Kim (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
